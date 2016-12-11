e720505a536b7e77d9a222ecaedb4bafb3ac0c30
Zeno Pelgrims <zenopelgrims@gmail.com>
2016-12-11 21:07:50 +0100

    cleanup

--- c4d/res/description/aitag_zoic.h
+++ c4d/res/description/aitag_zoic.h
@@ -5,10 +5,10 @@ enum
 {
    C4DAI_ZOIC_MAIN_GRP                                = 2001,
    C4DAI_ZOIC_GENERAL_GRP                             = 3001,
-   C4DAI_ZOIC_IMAGEBOKEH_GRP                          = ####,
-   C4DAI_ZOIC_RAYTRACED_GRP                           = ####,
-   C4DAI_ZOIC_THINLENS_GRP                            = ####,
-   C4DAI_ZOIC_EXTRA_GRP                               = 4001,
+   C4DAI_ZOIC_IMAGEBOKEH_GRP                          = 4001,
+   C4DAI_ZOIC_RAYTRACED_GRP                           = 5001,
+   C4DAI_ZOIC_THINLENS_GRP                            = 6001,
+   C4DAI_ZOIC_EXTRA_GRP                               = 7001,
 
    C4DAIP_ZOIC_SENSORWIDTH                            = 1184180162,
    C4DAIP_ZOIC_SENSORHEIGHT                           = 168369253,
@@ -17,14 +17,14 @@ enum
    C4DAIP_ZOIC_FOCALDISTANCE                          = 819210152,
    C4DAIP_ZOIC_USEIMAGE                               = 717433016,
    C4DAIP_ZOIC_BOKEHPATH                              = 1001292574,
-   C4DAIP_ZOIC_LENSMODEL                              = ##########,
-   C4DAIP_ZOIC_LENSDATAPATH                           = ##########,
-   C4DAIP_ZOIC_KOLBSAMPLINGLUT                        = ##########,
+   C4DAIP_ZOIC_LENSMODEL                              = 819210154,
+   C4DAIP_ZOIC_LENSDATAPATH                           = 819210155,
+   C4DAIP_ZOIC_KOLBSAMPLINGLUT                        = 819210156,
    C4DAIP_ZOIC_USEDOF                                 = 1290326798,
 
    
    C4DAIP_ZOIC_OPTICALVIGNETTINGDISTANCE              = 158969043,
-   C4DAIP_ZOIC_OPTICALVIGNETTINGRADIUS                = ##########,
+   C4DAIP_ZOIC_OPTICALVIGNETTINGRADIUS                = 819210157,
    C4DAIP_ZOIC_HIGHLIGHTWIDTH                         = 1715727008,
    C4DAIP_ZOIC_HIGHLIGHTSTRENGTH                      = 1587378065,
    C4DAIP_ZOIC_EXPOSURECONTROL                        = 1841413956,
